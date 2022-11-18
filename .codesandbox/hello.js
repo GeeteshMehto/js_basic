21faad3a963cbe627e4aa07965baa5024d28dbea
Geetesh Mehto <geeteshmehto018@gmail.com>
2022-11-18 09:37:21 +0000

    Update hello.js

--- .codesandbox/hello.js
+++ .codesandbox/hello.js
@@ -1,13 +1,10 @@
 const cowsay = require("cowsay");
+const Quote = require("inspirational-quotes");
 
-console.log(cowsay.say({
-    text : "I'm a moooodule",
-    e : "oO",
-    T : "U "
-}));
-
-const Quote = require('inspirational-quotes');
-
-console.log(Quote.getQuote()); // returns quote (text and author)
-console.log(Quote.getQuote({ author: false })); // return quote without author
-console.log(Quote.getRandomQuote()); // return any random quote
+console.log(
+  cowsay.say({
+    text: Quote.getRandomQuote(),
+    e: "oO",
+    T: "U ",
+  })
+);
